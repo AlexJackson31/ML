8cc033eeb1bdec46e3f8df096369cf9d032e5da2
Vidhi Joshi <58899273+AlexJackson31@users.noreply.github.com>
2019-12-21 16:59:04 +0530

    Add files via upload

---
+++ url.py
@@ -0,0 +1,15 @@
+class Short:
+    url={}
+    id=100
+    def shortenurl(self,original_url):
+        if original_url in self.url:
+            id=self.url[original_url]
+        else:
+            self.url[original_url]=self.id
+            self.id+=1
+            return self.id
+u=input("Enter the url:")
+u=Short()
+n=u.shortenurl(u)
+str1="short_url.com/"+str(n)
+print("Short url is:",str1)
